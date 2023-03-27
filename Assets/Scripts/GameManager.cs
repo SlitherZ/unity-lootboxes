84c427e240bb8fa13553aff9ea312a2863451a91
SlitherZ <slithertuftds@gmail.com>
2023-03-27 19:18:14 +0200

    1.2
    Now you need to put the options in prizestext.txt in Prizes
    
    the format:
    1 : Option A
    1 are the chances of the object appearing and Option A is the name of the object.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine.UI;
 using UnityEngine;
 
@@ -8,20 +9,41 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private Text getText, prizesText;
-    public string[] prizeList;
+    public List<string> prizeList = new List<string>();
     public List<string> prizes = new List<string>();
+    public string filepath  = "/Assets/Prizes/prizestext.txt";
 
     public KeyCode pullCode;
+
+    void Start(){
+        ReadTextFile();
+    }
 
     void Update()
     {
         if(Input.GetKeyDown(pullCode)){
-            int prizeGet = Random.Range(0, prizeList.Length);
-            string textedPrize = prizeList[prizeGet];
+            string[] prizeListArray = prizeList.ToArray();
+            int prizeGet = Random.Range(0, prizeListArray.Length);
+            string textedPrize = prizeListArray[prizeGet];
             prizes.Add(textedPrize);
             getText.text = $"You get : {textedPrize}";
             prizesText.text += $"\n {textedPrize}";
         
         }
     }
+
+    void ReadTextFile() {
+        string[] lines = File.ReadAllLines(filepath);
+        foreach (string line in lines) {
+            string[] textReader = line.Split(":");
+
+            int repeat = int.Parse(textReader[0].Trim());
+            string prizeOpt = textReader[1].Trim();
+
+            for(int i = 0; i > repeat;i++){
+                prizeList.Add(prizeOpt);
+            }
+        
+        } 
+}
 }
